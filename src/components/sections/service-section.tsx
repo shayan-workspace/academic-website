7106c6e15f581ce1ef2b7c8fe22238a116224cbb
MubashirJ <mubashirmjjawed@gmail.com>
2024-04-20 01:00:29 +0500

    service section

--- src/components/sections/service-section.tsx
+++ src/components/sections/service-section.tsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { Galada } from "next/font/google";
+import Link from "../../../node_modules/next/link";
 
 const galada = Galada({ subsets: ["latin"], weight: ["400"] });
 
 const ServiceSection = () => {
   return (
-    <div className=" bg-emerald-700 h-[100vh] w-[100%] flex justify-center items-center">
-      <div className="border w-[90%] h-[90%] flex justify-center items-center flex-col gap-5">
+    <div className=" py-20 bg-emerald-700  w-[100%] flex justify-center items-center">
+      <div className=" w-[90%] h-[90%] flex justify-center items-center flex-col gap-5">
         <span className="text-xl text-center text-white">{data.title}</span>
         <span
           style={galada.style}
           className="w-[60%] text-center italic font-bold text-6xl text-amber-500">
           {data.head}
         </span>
-        <span className="border w-[90%]">
-            {data.serviecs.map((val,i)=>(
-                <div key={i}>
-
-                </div>
-            ))}
+        <span className=" w-[90%] flex flex-wrap justify-center items-center gap-5 ">
+          {data.serviecs.map((val, i) => (
+            <div
+              key={i}
+              className="rounded-lg flex flex-col items-start gap-5 w-[370px] min-h-[300px] p-8 bg-yellow-50">
+              <span className="text-amber-600">{val.icon}</span>
+              <span className="text-3xl font-semibold">{val.name}</span>
+              <span className="text-lg">{val.txt}</span>
+              <Link
+                className="border rounded-full p-3 px-8 text-white text-lg font-normal bg-emerald-700 duration-500 hover:scale-105"
+                href={val.url}>
+                {val.btn}
+              </Link>
+            </div>
+          ))}
         </span>
       </div>
     </div>
